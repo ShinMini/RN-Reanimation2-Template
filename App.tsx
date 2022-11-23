13d00057b885f4e3ca8143d27b1a92c23ac8bd08
Hyeon Min <gusals121234@gmail.com>
2022-11-23 12:41:49 +0900

    [INIT] flat blur image

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/** @format */
+
+import {StatusBar} from 'expo-status-bar'
+import {Animated, StyleSheet, Text, View} from 'react-native'
+
+// user components
+import HorizontalView from './src/components/HorizontalView'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <Animated.View style={styles.container}>
+            <StatusBar hidden />
+            <HorizontalView />
+        </Animated.View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1
+    }
+})
